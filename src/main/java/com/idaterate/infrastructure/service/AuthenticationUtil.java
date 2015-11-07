a797fe34c37d689118198825681cd20c19ec5fb1
joeydeluca <joeydeluca@hotmail.com>
2015-11-07 10:16:25 -0500

    login

--- src/main/java/com/idaterate/infrastructure/service/AuthenticationUtil.java
+++ src/main/java/com/idaterate/infrastructure/service/AuthenticationUtil.java
@@ -1,11 +1,9 @@
 package com.idaterate.infrastructure.service;
 
-import com.sun.org.apache.xml.internal.security.exceptions.Base64DecodingException;
-import com.sun.org.apache.xml.internal.security.utils.Base64;
-
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Base64;
 
 /**
  * Created by Joe Deluca on 05/11/2015.
@@ -14,7 +12,7 @@ public class AuthenticationUtil {
     private final static String COOKIE_NAME = "daterateauth";
 
     public static void createToken(String username, HttpServletResponse response) {
-        Cookie cookie = new Cookie(COOKIE_NAME, Base64.encode(username.getBytes()));
+        Cookie cookie = new Cookie(COOKIE_NAME, Base64.getEncoder().encodeToString(username.getBytes()));
         response.addCookie(cookie);
     }
 
@@ -23,13 +21,9 @@ public class AuthenticationUtil {
 
         for(Cookie cookie : request.getCookies()) {
             if(COOKIE_NAME.equals(cookie.getName())) {
-                if(cookie.getValue() != null && cookie.getValue().length() > 0)
-                    try {
-                        return new String(Base64.decode(cookie.getValue()));
-                    } catch (Base64DecodingException e) {
-                        e.printStackTrace();
-                        return null;
-                    }
+                if(cookie.getValue() != null && cookie.getValue().length() > 0) {
+                    return new String(Base64.getDecoder().decode(cookie.getValue()));
+                }
             }
         }
 
--- src/main/java/com/idaterate/service/AuthenticationService.java
+++ src/main/java/com/idaterate/service/AuthenticationService.java
@@ -3,14 +3,9 @@ package com.idaterate.service;
 import com.idaterate.domain.User;
 import com.idaterate.infrastructure.repositories.IUserRepository;
 import com.idaterate.infrastructure.service.CryptographyUtil;
-import com.sun.org.apache.xml.internal.security.exceptions.Base64DecodingException;
-import com.sun.org.apache.xml.internal.security.utils.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
 
